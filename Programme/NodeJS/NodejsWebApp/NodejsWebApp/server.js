676c535ef7b9b694c32fbceeb8850110e6658c98
akcaemre <emre_a1@hotmail.com>
2018-01-11 19:20:36 +0100

    Updated files from local to Git

--- Programme/NodeJS/NodejsWebApp/NodejsWebApp/server.js
+++ Programme/NodeJS/NodejsWebApp/NodejsWebApp/server.js
@@ -14,10 +14,16 @@ var psw = "2OMWwM8lYJ6yJITc";
 var uri = "mongodb://" + user + ":" + psw + "@cluster0-shard-00-00-xguqy.mongodb.net:27017,cluster0-shard-00-01-xguqy.mongodb.net:27017,cluster0-shard-00-02-xguqy.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
 
 var port = process.env.port || 1337;
+console.log("Running on port: " + port);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  
 app.get('/', function (req, res) {
     res.send("NodeJS WebService with MongoDB");
     res.end(200);
@@ -35,7 +41,7 @@ app.put('/update', function (req, res) {
     update(req, res, collection);
 });
 
-app.delete('/delete', function (req, res) {
+app.post('/delete', function (req, res) {
     remove(req, res, collection);
 });
 
@@ -43,14 +49,23 @@ app.listen(port);
 
 function read(res, collection) {
     MongoClient.connect(uri, function (err, client) {
-        if (err) throw err;
-        var db = client.db(database);
-        db.collection(collection).find({}).toArray(function (err, result) {
-            if (err) throw err;
-            res.send(result);
-            res.end(200);
-        });
-        client.close();
+        if (err) {
+            res.send(err);
+            res.end(400);
+        } else {
+
+            var db = client.db(database);
+            db.collection(collection).find({}).toArray(function (err, result) {
+                if (err) {
+                    res.send(err);
+                    res.end(400);
+                } else {
+                    res.send(result);
+                    res.end(200);
+                }
+            });
+            client.close();
+        }
     });
 }
 
@@ -58,50 +73,72 @@ function create(req, res, collection) {
     var jsonInsert = req.body;
     
     MongoClient.connect(uri, function (err, client) {
-        if (err) throw err;
-        var db = client.db(database);
+        if (err) {
+            res.send(err);
+            res.end(400);
+        } else {
+            var db = client.db(database);
 
-        db.collection(collection).insertOne(jsonInsert, function (err, result) {
-            if (err) {
-                res.send(err);
-                res.end(400);
-            } else {
-                res.send("Inserted 1 document");
-                res.end(200);
-            }
-        });
-        client.close();
+            db.collection(collection).insertOne(jsonInsert, function (err, result) {
+                if (err) {
+                    res.send(err);
+                    res.end(400);
+                } else {
+                    res.send("Inserted 1 document");
+                    res.end(200);
+                }
+            });
+            client.close();
+        }
     });
 }
 
-// TODO: UPDATE !!!! 
 function update(req, res, collection) {
-    MongoClient.connect(url, function (err, client) {
-        if (err) throw err;
+    var myquery = req.body;
+    var sourceValues = myquery[0].source;
+    var newValues = myquery[0].destination;
 
-        var db = client.db(database);
-        var myquery = { address: "Valley 345" };
-        var newvalues = { name: "Mickey", address: "Canyon 123" };
+    MongoClient.connect(uri, function (err, client) {
+        if (err) {
+            res.send(err);
+            res.end(400);
+        } else {
+            var db = client.db(database);
 
-        db.collection("customers").updateOne(myquery, newvalues, function (err, res) {
-            if (err) throw err;
-            console.log("1 document updated");
+            db.collection(collection).updateOne(sourceValues, newValues /*{ name: sourceValues.name }, { $set: newValues }*/, function (err, dbresp) {
+                if (err) {
+                    res.send(err);
+                    res.end(400);
+                } else {
+                    res.send("1 document updated");
+                    res.end(200);
+                }
+            });
             client.close();
-        });
+        }
     });
 }
 
 function remove(req, res, collection) {
-    MongoClient.connect(url, function (err, client) {
-        if (err) throw err;
+    MongoClient.connect(uri, function (err, client) {
+        if (err) {
+            res.send(err);
+            res.end(400);
+        } else {
 
-        var db = client.db(database);
-        var myquery = req.body;
-        db.collection("customers").deleteOne(myquery, function (err, obj) {
-            if (err) throw err;
-            res.send("1 document deleted");
-            res.end(200);
+            var db = client.db(database);
+            var myquery = req.body;
+
+            db.collection(collection).deleteOne(myquery, function (err, obj) {
+                if (err) {
+                    res.send(err);
+                    res.end(400);
+                } else {
+                    res.send("1 document deleted");
+                    res.end(200);
+                }
+            });
             client.close();
-        });
+        }
     });
 }
--- Programme/NodeJS/NodejsWebApp/NodejsWebApp/server1.js
+++ Programme/NodeJS/NodejsWebApp/NodejsWebApp/server1.js
@@ -1,81 +1,141 @@
 ﻿var http = require('http');
 var url = require('url');
+
+var express = require('express');
+var app = express();
+
+var bodyParser = require('body-parser');
+
 var MongoClient = require('mongodb').MongoClient;
-
-var uri = "mongodb://admin:2OMWwM8lYJ6yJITc@cluster0-shard-00-00-xguqy.mongodb.net:27017,cluster0-shard-00-01-xguqy.mongodb.net:"
-    + "27017,cluster0-shard-00-02-xguqy.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
-   // "mongodb+srv://admin:2OMWwM8lYJ6yJITc@cluster0-xguqy.mongodb.net/TestDB";
-var port = process.env.port || 1337;
 var collection = "MyFirstCollection";
+var database = "TestDB";
+var user = "admin";
+var psw = "2OMWwM8lYJ6yJITc";
+var uri = "mongodb://" + user + ":" + psw + "@cluster0-shard-00-00-xguqy.mongodb.net:27017,cluster0-shard-00-01-xguqy.mongodb.net:27017,cluster0-shard-00-02-xguqy.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
 
-MongoClient.connect(uri, function (err, client) {
-    if (err) throw err;
-    var db = client.db('TestDB');
-    db.collection("MyFirstCollection").find({}).toArray(function (err, result) {
-        if (err) throw err;
-        console.log(result);
-    });
-    client.close();
+var port = process.env.port || 1337;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+    res.send("NodeJS WebService with MongoDB");
+    res.end(200);
 });
 
-http.createServer(function (req, res) {
-    var redirect = req.url.split("?")[0];
-    var response = "";
-    var responseCode = 200;
-    
-    switch (redirect) {
-        case "/create":
-            response = create(url.parse(req.url, true).query);
-            break;
-        case "/read":
-            response = read();
-            break;
-        case "/update":
-            response = update();
-            break;
-        case "/delete":
-            response = remove();
-            break;
-        default:
-            response = "URL not found";
-            responseCode = 404;
-            break;
-    }
+app.get('/read', function (req, res) {
+    read(res, collection);
+});
 
-    res.writeHead(responseCode, { 'Content-Type': 'text/plain' });
-    res.end(response);
-}).listen(port);
+app.post('/create', function (req, res) {
+    create(req, res, collection);
+});
 
-function read() {
-    var response = "";
+app.put('/update', function (req, res) {
+    update(req, res, collection);
+});
 
+app.post('/delete', function (req, res) {
+    remove(req, res, collection);
+});
+
+app.listen(port);
+
+function read(res, collection) {
     MongoClient.connect(uri, function (err, client) {
-        if (err) throw err;
-        var db = client.db('TestDB');
-        db.collection(collection).find({}).toArray(function (err, result) {
-            if (err) throw err;
-            response = result;
-        });
-        client.close();
-    });
+        if (err) {
+            res.send(err);
+            res.end(400);
+        } else {
 
-    return response;
+            var db = client.db(database);
+            db.collection(collection).find({}).toArray(function (err, result) {
+                if (err) {
+                    res.send(err);
+                    res.end(400);
+                } else {
+                    res.send(result);
+                    res.end(200);
+                }
+            });
+            client.close();
+        }
+    });
 }
 
-function create(q) {
-    var response = "";
-
-    response = JSON.stringify(q);
-
+function create(req, res, collection) {
+    var jsonInsert = req.body;
+    
     MongoClient.connect(uri, function (err, client) {
-        if (err) throw err;
-        var db = client.db('TestDB');
-        db.collection(collection).insertOne(response, function (err, res) {
-            if (err) throw err;
-            console.log("1 document inserted");
-        });
-        client.close();
-    });
+        if (err) {
+            res.send(err);
+            res.end(400);
+        } else {
+            var db = client.db(database);
 
-    return response;
+            db.collection(collection).insertOne(jsonInsert, function (err, result) {
+                if (err) {
+                    res.send(err);
+                    res.end(400);
+                } else {
+                    res.send("Inserted 1 document");
+                    res.end(200);
+                }
+            });
+            client.close();
+        }
+    });
+}
+
+function update(req, res, collection) {
+    MongoClient.connect(uri, function (err, client) {
+        if (err) {
+            res.send(err);
+            res.end(400);
+        } else {
+
+            var db = client.db(database);
+            var myquery = req.body;
+
+            console.log("Body " + myquery);
+
+            var sourceValues = myquery[0].source;
+            var newValues = myquery[0].destination;
+
+            db.collection(collection).updateOne(sourceValues, newValues, function (err, dbresp) {
+                if (err) {
+                    res.send(err);
+                    res.end(400);
+                } else {
+                    res.send("1 document updated");
+                    res.end(200);
+                }
+            });
+            client.close();
+        }
+    });
+}
+
+function remove(req, res, collection) {
+    MongoClient.connect(uri, function (err, client) {
+        if (err) {
+            res.send(err);
+            res.end(400);
+        } else {
+
+            var db = client.db(database);
+            var myquery = req.body;
+
+            db.collection(collection).deleteOne(myquery, function (err, obj) {
+                if (err) {
+                    res.send(err);
+                    res.end(400);
+                } else {
+                    res.send("1 document deleted");
+                    res.end(200);
+                }
+            });
+            client.close();
+        }
+    });
 }
